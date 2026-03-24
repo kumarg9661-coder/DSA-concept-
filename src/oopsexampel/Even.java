e24b9bc014a561998e941b349781dd160fc6b465
Kumar Gaurav bharti <kumarg9661@gmail.com>
2026-03-24 15:56:09 +0530

    added even

--- src/oopsexampel/Even.java
+++ src/oopsexampel/Even.java
@@ -1,4 +1,31 @@
-package oopsexampel;
+import java.util.Scanner;
 
-public class Evennumber {
+// Utility class for logic
+class Even{
+    public static boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+}
+
+// Main class (entry point)
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+
+        // Check if input is valid integer
+        if (sc.hasNextInt()) {
+            int num = sc.nextInt();
+
+            if (Even.isEven(num)) {
+                System.out.println("Even number");
+            } else {
+                System.out.println("Odd number");
+            }
+        } else {
+            System.out.println("Invalid input. Please enter an integer.");
+        }
+
+        sc.close();
+    }
 }
