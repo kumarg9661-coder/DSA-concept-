7c50d55d12e9126bb7fef9953c22f457be7bc145
Kumar Gaurav bharti <kumarg9661@gmail.com>
2026-03-28 22:41:52 +0530

    polymorphism example

--- src/oopsexampel/PolymorphismEx2.java
+++ src/oopsexampel/PolymorphismEx2.java
@@ -1,4 +1,21 @@
 package oopsexampel;
 
-public class PolymorphismEx2 {
+public class PolymorphismEx2{
+    public static class Animal {
+        void sound() {
+            System.out.println("Animal makes sound");
+        }
+    }
+
+    public static class Dog extends Animal {
+        @Override
+        void sound() {
+            System.out.println("Dog barks");
+        }
+
+        public static void main(String[] args) {
+            Animal obj = new Dog();
+            obj.sound();
+        }
+    }
 }
